68895be6b0d82a73f28ab5bb80c9675d485bc8ef
Alisa Stoten <92325819+alisastoten@users.noreply.github.com>
2022-11-15 01:24:31 +0200

    Created flying object exercise

---
+++ FlyingObject/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace FlyingObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_flying = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label_flying
+            // 
+            this.label_flying.AutoSize = true;
+            this.label_flying.Location = new System.Drawing.Point(59, 48);
+            this.label_flying.Name = "label_flying";
+            this.label_flying.Size = new System.Drawing.Size(86, 17);
+            this.label_flying.TabIndex = 0;
+            this.label_flying.Text = "Make me fly!";
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_start.Location = new System.Drawing.Point(226, 226);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(103, 36);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_stop.Location = new System.Drawing.Point(369, 226);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(103, 36);
+            this.button_stop.TabIndex = 2;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 419);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_flying);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_flying;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FlyingObject/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyingObject
+{
+    public partial class Form1 : Form
+    {
+        private int leftStep = 70;
+        private int downStep = 50;
+        public Form1()
+        {
+            InitializeComponent();
+            //timer interval should be 0.4s 
+            timer1.Interval = 200; // 0.4 * 1000
+            label_flying.Left = leftStep;
+            label_flying.Top = downStep;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            label_flying.Text = "I'm flying!";
+            timer1.Start();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            label_flying.Text = "I stopped :(";
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // all clockwise moving logic should be here
+
+            // how far from the left + extra length from the label itself
+            int distanceFromLeft = label_flying.Left + label_flying.Width;
+            int distanceFromTop = label_flying.Top + label_flying.Height;
+
+            if (Width - distanceFromLeft > 150 && label_flying.Top < 100)
+            {
+                label_flying.Left += leftStep;
+            }
+            else if (Height - distanceFromTop > 100 && Width - distanceFromLeft <= 150)
+            {
+                label_flying.Top += downStep;
+            }
+            else if (label_flying.Left > 100)
+            {
+                label_flying.Left -= leftStep;
+            }
+            else
+            {
+                label_flying.Top -= downStep;
+            }
+
+
+        }
+    }
+}
